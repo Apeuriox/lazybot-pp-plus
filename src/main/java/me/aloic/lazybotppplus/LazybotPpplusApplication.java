bcc477753304504ff00fbf42b4d01d98ef4d2396
Aloic <liuxusiu@foxmail.com>
2025-07-15 14:36:44 +0800

    added missing annotation im kind of dumb here

--- src/main/java/me/aloic/lazybotppplus/LazybotPpplusApplication.java
+++ src/main/java/me/aloic/lazybotppplus/LazybotPpplusApplication.java
@@ -2,14 +2,14 @@ package me.aloic.lazybotppplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LazybotPpplusApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(LazybotPpplusApplication.class, args);
     }
-
 }
