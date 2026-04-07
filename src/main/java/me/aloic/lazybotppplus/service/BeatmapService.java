0c27a12223e4d23752f2bf1d6ccaf81b2c243940
Aloic <liuxusiu@foxmail.com>
2026-04-07 23:30:36 +0800

    fixed beatmap update being transactional

--- src/main/java/me/aloic/lazybotppplus/service/BeatmapService.java
+++ src/main/java/me/aloic/lazybotppplus/service/BeatmapService.java
@@ -1,9 +1,7 @@
 package me.aloic.lazybotppplus.service;
 
-import org.springframework.transaction.annotation.Transactional;
 
 public interface BeatmapService
 {
-    @Transactional
     void updatedBeatmapCache();
 }
